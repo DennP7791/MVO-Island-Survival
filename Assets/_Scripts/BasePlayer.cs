c46a853fbd41fe7d6f02597042a298c1b7b9e698
DennP7791 <cookie070791@gmail.com>
2016-05-06 09:43:41 +0200

    new terrain (unfinished)

---
+++ Assets/_Scripts/BasePlayer.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasePlayer {
+
+    public string Name { get; set; }
+
+    public int Health { get; set; }
+    public int Energy { get; set; }
+    public int Stamina { get; set; }
+    public int Hunger { get; set; }
+    public int Thirst { get; set; }
+    public int Temperature { get; set; }
+
+    public int MaxHealth { get; set; }
+    public int MaxEnergy { get; set; }
+    public int MaxStamina { get; set; }
+    public int MaxHunger { get; set; }
+    public int MaxThirst { get; set; }
+    public int MaxTemperature { get; set; }
+
+    public BasePlayer(string name)
+    {
+        this.Name = name;
+        this.MaxHealth = 100;
+        this.Health = MaxHealth;
+        this.MaxEnergy = 100;
+        this.Energy = MaxEnergy;
+        this.MaxStamina = 100;
+        this.Stamina = MaxStamina;
+        this.MaxHunger = 100;
+        this.Hunger = MaxHunger;
+        this.MaxThirst = 100;
+        this.Thirst = MaxThirst;
+        this.MaxTemperature = 100;
+        this.Temperature = MaxTemperature;
+    }
+
+}
---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerBehaviour {
+
+    public BasePlayer player;
+    private Text playerHealthUI;
+
+    public PlayerBehaviour(BasePlayer basePlayer)
+    {
+        this.player = basePlayer;
+        playerHealthUI = GameObject.Find("PlayerHealthPoints").GetComponent<Text>();
+    }
+
+    public void UpdatePlayerStats()
+    {
+        UpdatePlayerUI();
+    }
+
+    public void UpdatePlayerUI()
+    {
+        playerHealthUI.text = "Health Points: " + player.Health + "/" + player.MaxHealth;
+    }
+}
